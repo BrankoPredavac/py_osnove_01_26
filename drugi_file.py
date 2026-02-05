00c7eedabace3ace98dc9f52da81b5f984d4cf09
Branko <branko.rados@predavaci.algebra.hr>
2026-02-05 21:07:36 +0100

    Tipovi podataka ispisanisvanje

--- drugi_file.py
+++ drugi_file.py
@@ -5,11 +5,15 @@ Ovaj file pokazuje kako korisiti varijable
 MOJE_IME = "Branko"
 BROJ_GODINA = 42
 MOJA_TEZINA_KG = 120
+print(type(MOJA_TEZINA_KG))
 
 print("Zdebljo sam se")
 MOJA_TEZINA_KG = "Sto dvadeset"
+print(type(MOJA_TEZINA_KG))
 MOJA_TEZINA_KG = True
+print(type(MOJA_TEZINA_KG))
 MOJA_TEZINA_KG = 77.35232
+print(type(MOJA_TEZINA_KG))
 
 
 print("GOTOVO")
