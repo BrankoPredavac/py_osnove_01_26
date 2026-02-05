5d55016fd0a565302ca231a54ffe642d6c97c4b4
Branko <branko.rados@predavaci.algebra.hr>
2026-02-05 20:57:59 +0100

    Promjena tezine

--- drugi_file.py
+++ drugi_file.py
@@ -4,7 +4,10 @@ Ovaj file pokazuje kako korisiti varijable
 
 MOJE_IME = "Branko"
 BROJ_GODINA = 42
-MOJA_TEZINA = 120
+MOJA_TEZINA_KG = 120
+
+print("Zdebljo sam se")
+MOJA_TEZINA_KG = 140
 
 
 print("GOTOVO")
