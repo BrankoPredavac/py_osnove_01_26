0107fed474e78c341dec772556fba106238c0d3f
Branko <branko.rados@predavaci.algebra.hr>
2026-02-05 21:01:08 +0100

    varijabla tip agnostik

--- drugi_file.py
+++ drugi_file.py
@@ -7,7 +7,9 @@ BROJ_GODINA = 42
 MOJA_TEZINA_KG = 120
 
 print("Zdebljo sam se")
-MOJA_TEZINA_KG = 140
+MOJA_TEZINA_KG = "Sto dvadeset"
+MOJA_TEZINA_KG = True
+MOJA_TEZINA_KG = 77.35232
 
 
 print("GOTOVO")
