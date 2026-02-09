d1433ffc90047985b16aa61e210b7ad225829ece
Branko <branko.rados@predavaci.algebra.hr>
2026-02-09 18:20:56 +0100

    printanje vrijednosti

--- drugi_file.py
+++ drugi_file.py
@@ -6,14 +6,20 @@ MOJE_IME = "Branko"
 BROJ_GODINA = 42
 MOJA_TEZINA_KG = 120
 print(type(MOJA_TEZINA_KG))
+print(MOJA_TEZINA_KG)
 
 print("Zdebljo sam se")
 MOJA_TEZINA_KG = "Sto dvadeset"
 print(type(MOJA_TEZINA_KG))
+print(MOJA_TEZINA_KG)
+
 MOJA_TEZINA_KG = True
 print(type(MOJA_TEZINA_KG))
+
+
 MOJA_TEZINA_KG = 77.35232
 print(type(MOJA_TEZINA_KG))
+print(MOJA_TEZINA_KG)
 
 
 print("GOTOVO")
