925504497aa822754ee881ff66d41a2395df1f31
Branko <branko.rados@predavaci.algebra.hr>
2026-02-09 18:11:42 +0100

    pokazivanje memorijskih lokaciaj gdje su pohranjenje vrijednosti

--- primjer_3.py
+++ primjer_3.py
@@ -1,6 +1,10 @@
 cijena_bez_pdv_eur = 200
-pdv = 0.25
 
+print(id(cijena_bez_pdv_eur))
+
+pdv = 0.25
+print(id(pdv))
 
 cijena_s_pdv_eur = cijena_bez_pdv_eur + cijena_bez_pdv_eur * pdv
 print(cijena_s_pdv_eur)
+print(id(cijena_s_pdv_eur))
